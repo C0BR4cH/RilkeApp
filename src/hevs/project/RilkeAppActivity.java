901b7438a762231975c017e9590d967e8424009b
COBRA <huri_m@hotmail.com>
2012-01-10 15:43:38 +0100

    First Commit

---
+++ src/hevs/project/RilkeAppActivity.java
@@ -0,0 +1,13 @@
+package hevs.project;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RilkeAppActivity extends Activity
+{
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
