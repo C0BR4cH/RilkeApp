4f71a484b48acc9c6ef67322a44ce7b1591be6f1
COBRA <huri_m@hotmail.com>
2012-01-10 18:34:29 +0100

    Rename Activity Rename values Add values-fr Edit Activity

--- src/hevs/project/MainMenu.java
+++ src/hevs/project/MainMenu.java
@@ -3,11 +3,11 @@ package hevs.project;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class RilkeAppActivity extends Activity
+public class MainMenu extends Activity
 {
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.main_menu);
     }
 }
