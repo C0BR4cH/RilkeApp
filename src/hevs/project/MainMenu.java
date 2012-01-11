4ad5f6a421a5cc371f2b0f21a8b78472ee7bf3cf
COBRA <huri_m@hotmail.com>
2012-01-11 15:40:04 +0100

    Menu -> Implement Quiz

--- src/hevs/project/MainMenu.java
+++ src/hevs/project/MainMenu.java
@@ -1,7 +1,9 @@
 package hevs.project;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainMenu extends Activity
 {
@@ -9,5 +11,14 @@ public class MainMenu extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_menu);
+    }
+    
+    public void onClick(View v)
+    {
+    	if(v==findViewById(R.id.btnQuizz))
+    	{
+			Intent settings = new Intent(MainMenu.this,Quiz.class);
+			MainMenu.this.startActivity(settings);
+    	}
     }
 }
