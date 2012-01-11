4e4981b142b497078dbf9fd1508c11b898809b78
COBRA <huri_m@hotmail.com>
2012-01-11 14:28:56 +0100

    Add Quiz Activity + Layout

--- gen/hevs/project/R.java
+++ gen/hevs/project/R.java
@@ -19,16 +19,28 @@ public final class R {
         public static final int button3=0x7f050009;
         public static final int button4=0x7f05000b;
         public static final int imageView1=0x7f050002;
+        public static final int layoutQuiz=0x7f05000c;
         public static final int reNews=0x7f050004;
+        public static final int rwAnswerA=0x7f050011;
+        public static final int rwAnswerB=0x7f050013;
+        public static final int rwAnswerC=0x7f050015;
+        public static final int rwImage=0x7f050010;
         public static final int rwQuizz=0x7f050006;
+        public static final int rwTitle=0x7f05000d;
         public static final int tableLayout1=0x7f050000;
         public static final int tableRow1=0x7f050001;
         public static final int tableRow4=0x7f05000a;
         public static final int tableRow5=0x7f050008;
         public static final int textView1=0x7f050003;
+        public static final int txtAnswerA=0x7f050012;
+        public static final int txtAnswerB=0x7f050014;
+        public static final int txtAnswerC=0x7f050016;
+        public static final int txtScore=0x7f05000f;
+        public static final int txtTitle=0x7f05000e;
     }
     public static final class layout {
         public static final int main_menu=0x7f030000;
+        public static final int quiz=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
---
+++ src/hevs/project/Quiz.java
@@ -0,0 +1,22 @@
+/* RilkeApp
+ * 
+ * 
+ * Quiz.java
+ * 
+ * Matthias Hurni
+ * Created: 11.01.2012 14:04:17
+ * Edit: 
+ */
+package hevs.project;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Quiz extends Activity
+{
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.quiz);
+    }
+}
