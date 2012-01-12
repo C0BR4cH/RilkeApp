a608989d5fdeefb72472df4b035499cf70fc235c
COBRA <huri_m@hotmail.com>
2012-01-12 14:19:26 +0100

    Edit QuizLayout Edit QuizActivity

--- src/hevs/project/Quiz.java
+++ src/hevs/project/Quiz.java
@@ -11,12 +11,34 @@ package hevs.project;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Quiz extends Activity
 {
+	private TextView score;
+	private TextView question;
+	private TextView answerA;
+	private TextView answerB;
+	private TextView answerC;
+	private Button btnOk;
+	
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.quiz);
     }
+    
+    public class ButtonListener implements OnClickListener
+    {
+	    public void onClick(View v)
+	    {
+	    	if(v==findViewById(R.id.btnOk))
+	    	{
+	    		
+	    	}
+	    }    
+	}
 }
