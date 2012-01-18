6e425dee7ec428cb34f02e976bf4d73171ffc6a7
sharpblade <sharpblade@gmx.net>
2012-01-18 15:01:13 +0100

    damn r.java .project

--- gen/hevs/project/R.java
+++ gen/hevs/project/R.java
@@ -36,57 +36,63 @@ public final class R {
         public static final int rilke_plakat3=0x7f020008;
     }
     public static final class id {
-        public static final int GridView_Buttons=0x7f060002;
+        public static final int GridView_Buttons=0x7f060006;
         public static final int LinearLayout1=0x7f060000;
-        public static final int btnA=0x7f06000e;
-        public static final int btnAgain=0x7f060017;
-        public static final int btnB=0x7f06000f;
-        public static final int btnC=0x7f060010;
-        public static final int btnHome=0x7f060011;
-        public static final int btnRHome=0x7f060016;
-        public static final int imageView1=0x7f060009;
-        public static final int imgPlan=0x7f060018;
-        public static final int linearLayout2=0x7f060003;
-        public static final int myvideoview=0x7f060021;
-        public static final int story_StoryText=0x7f06001b;
-        public static final int story_StoryTitle=0x7f06001a;
-        public static final int story_Title=0x7f060019;
-        public static final int story_author=0x7f06001c;
-        public static final int story_btnHome=0x7f06001e;
-        public static final int story_btnNext=0x7f06001f;
-        public static final int story_btnPrev=0x7f06001d;
-        public static final int textView1=0x7f060001;
-        public static final int textView2=0x7f060004;
-        public static final int txtAnswerA=0x7f06000b;
-        public static final int txtAnswerB=0x7f06000c;
-        public static final int txtAnswerC=0x7f06000d;
-        public static final int txtCorrect=0x7f060006;
-        public static final int txtCorrectMax=0x7f060008;
-        public static final int txtCorrectNum=0x7f060007;
-        public static final int txtQuestion=0x7f06000a;
-        public static final int txtRCorrect=0x7f060013;
-        public static final int txtRCorrectMax=0x7f060015;
-        public static final int txtRCorrectNum=0x7f060014;
-        public static final int txtRTitle=0x7f060012;
-        public static final int txtTitle=0x7f060005;
-        public static final int vid_Title=0x7f060020;
-        public static final int vid_btnHome=0x7f060023;
-        public static final int vid_btnNext=0x7f060024;
-        public static final int vid_btnPrev=0x7f060022;
+        public static final int btnA=0x7f060012;
+        public static final int btnAgain=0x7f06001b;
+        public static final int btnB=0x7f060013;
+        public static final int btnC=0x7f060014;
+        public static final int btnHome=0x7f060015;
+        public static final int btnRHome=0x7f06001a;
+        public static final int gallery_Title=0x7f060001;
+        public static final int gallery_btnHome=0x7f060004;
+        public static final int galley_gall=0x7f060003;
+        public static final int imageView1=0x7f06000d;
+        public static final int imgPlan=0x7f06001c;
+        public static final int linearLayout2=0x7f060007;
+        public static final int myvideoview=0x7f060025;
+        public static final int story_StoryText=0x7f06001f;
+        public static final int story_StoryTitle=0x7f06001e;
+        public static final int story_Title=0x7f06001d;
+        public static final int story_author=0x7f060020;
+        public static final int story_btnHome=0x7f060022;
+        public static final int story_btnNext=0x7f060023;
+        public static final int story_btnPrev=0x7f060021;
+        public static final int test_image=0x7f060002;
+        public static final int textView1=0x7f060005;
+        public static final int textView2=0x7f060008;
+        public static final int txtAnswerA=0x7f06000f;
+        public static final int txtAnswerB=0x7f060010;
+        public static final int txtAnswerC=0x7f060011;
+        public static final int txtCorrect=0x7f06000a;
+        public static final int txtCorrectMax=0x7f06000c;
+        public static final int txtCorrectNum=0x7f06000b;
+        public static final int txtQuestion=0x7f06000e;
+        public static final int txtRCorrect=0x7f060017;
+        public static final int txtRCorrectMax=0x7f060019;
+        public static final int txtRCorrectNum=0x7f060018;
+        public static final int txtRTitle=0x7f060016;
+        public static final int txtTitle=0x7f060009;
+        public static final int vid_Title=0x7f060024;
+        public static final int vid_btnHome=0x7f060027;
+        public static final int vid_btnNext=0x7f060028;
+        public static final int vid_btnPrev=0x7f060026;
     }
     public static final class layout {
-        public static final int main_menu=0x7f030000;
-        public static final int quiz=0x7f030001;
-        public static final int quiz_result=0x7f030002;
-        public static final int sketch=0x7f030003;
-        public static final int stories_poetry=0x7f030004;
-        public static final int video=0x7f030005;
+        public static final int gallery=0x7f030000;
+        public static final int main_menu=0x7f030001;
+        public static final int quiz=0x7f030002;
+        public static final int quiz_result=0x7f030003;
+        public static final int sketch=0x7f030004;
+        public static final int stories_poetry=0x7f030005;
+        public static final int video=0x7f030006;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int gallery_Title=0x7f04000e;
+        public static final int gallery_iconDescription=0x7f040003;
         public static final int menu_Title=0x7f040001;
         public static final int menu_footer=0x7f040002;
-        public static final int menu_iconDescription=0x7f040003;
         public static final int poetry_Title=0x7f040008;
         public static final int quiz_again=0x7f04000b;
         public static final int quiz_correct=0x7f04000d;
@@ -98,4 +104,25 @@ public final class R {
         public static final int story_Title=0x7f040007;
         public static final int vid_Title=0x7f040009;
     }
+    public static final class styleable {
+        /** Attributes that can be used with a gallery_gallery.
+           <p>Includes the following attributes:</p>
+           <table>
+           <colgroup align="left" />
+           <colgroup align="left" />
+           <tr><th>Attribute</th><th>Description</th></tr>
+           <tr><td><code>{@link #gallery_gallery_android_galleryItemBackground hevs.project:android_galleryItemBackground}</code></td><td></td></tr>
+           </table>
+           @see #gallery_gallery_android_galleryItemBackground
+         */
+        public static final int[] gallery_gallery = {
+            0x0101004c
+        };
+        /**
+          <p>This symbol is the offset where the {@link hevs.project.R.attr#android_galleryItemBackground}
+          attribute's value can be found in the {@link #gallery_gallery} array.
+          @attr name android:android_galleryItemBackground
+        */
+        public static final int gallery_gallery_android_galleryItemBackground = 0;
+    };
 }
