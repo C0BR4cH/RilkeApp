f78f531d79b27ccba11a52c36223bf1e583ae122
COBRA <huri_m@hotmail.com>
2012-01-18 15:02:59 +0100

    Resize Images Add Route Activity

---
+++ src/hevs/project/Route.java
@@ -0,0 +1,22 @@
+/* RilkeApp
+ * 
+ * 
+ * Route.java
+ * 
+ * Matthias Hurni
+ * Created: 18.01.2012 14:52:59
+ * Edit: 
+ */
+package hevs.project;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Route extends Activity
+{
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.route);
+	}
+}
